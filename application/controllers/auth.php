6ec5a5503a67340a91780f1cfb9ab71e58fccd63
Phryel <phryels@gmail.com>
2023-02-24 20:11:26 +0700

    masih error

---
+++ application/controllers/auth.php
@@ -0,0 +1,21 @@
+<?php 
+defined('BASEPATH') OR exit ('No direct
+script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('templates/auth_footer');
+        $this->load->view('auth/login');
+    }
+
+    public function registrasi()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('templates/auth_footer');
+        $this->load->view('auth/registrasi'); 
+    }
+}
+?>
